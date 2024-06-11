4bf38c355167a3ba58d591d2d14b10d747a73da9
phantoan111 <phanthetoan3@gmail.com>
2024-06-11 14:28:14 +0700

    feat(frontend): add feature of frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './Header.css'
 const Header = () => {
   return (
-    <div>
-      <h1>Hello, This is header</h1>
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite pizza here</h2>
+        <p>You will create your favourite pizza from elements array below. First choose your favourite type of pizza. Second choose size this. Finally, you add topping. Yes this is pizza of your.</p>
+        <button>View Menu</button>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
 import './Home'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
+
+
 const Home = () => {
+    const [category, setCategory] = useState("All");
+    // const greet = () => {
+    //   const hello = React.createElement(
+    //     'p',
+    //     {
+    //       className:"greet"
+    //     },
+    //     "hello from Viet Nam"
+    //   )
+    //   const h1greet = React.createElement(
+    //     'h1',
+    //     {
+    //         className: "greet"
+    //     },
+    //     "hello from Viet Nam"
+    //   )
+    //   ReactDOM.render(h1greet,document.getElementById('demo'))
+    //   console.log(h1greet)
+    //   const container = document.getElementById('demo')
+    //   const demo = ReactDOM.createRoot(container);
+    //   demo.render(hello)
+    // }
+
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      {/* <button onClick={greet} id='demo'>hello</button> */}
+      <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
