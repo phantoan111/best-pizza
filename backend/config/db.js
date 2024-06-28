63d57647cb506481941db7b09e70a99ba86fa168
phantoan111 <102664952+phantoan111@users.noreply.github.com>
2024-06-28 19:37:40 +0700

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://admin-ken:root-123456@cluster0.yel0wg0.mongodb.net/demo-best-pizza').then(()=>console.log("Mongodb is connecting"));
+    await mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.yel0wg0.mongodb.net/demo-best-pizza`).then(()=>console.log("Mongodb is connecting"));
 }
